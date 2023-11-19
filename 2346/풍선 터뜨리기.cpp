2d86c2b2f1acebd4400e33f4b0e9fb311f2c48b3
Yongsullee <webdev_ys@kakao.com>
2023-11-19 13:28:21 +0900

    Feature : fix 2346, 풍선 터뜨리기(덱 활용) 문제 주석 추가해서 수정

--- 2346/풍선 터뜨리기.cpp
+++ 2346/풍선 터뜨리기.cpp
@@ -7,45 +7,51 @@ int main()
     ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
     
     int N;
-    deque<pair<int, int>> dq;
-    
     cin >> N;
+    
+    // pair< 인덱스, 회전 수 > 형식을 담은 덱 선언
+    deque<pair<int,int>> dq;
+    
     for(int i=1; i<=N; i++)
     {
         int tmp;
         cin >> tmp;
+        
         dq.push_back({i, tmp});
     }
     
     while(!dq.empty())
     {
-        cout << dq.front().first << ' ';
-        int cur = dq.front().second;
+        // #1. 덱 가장 앞에 위치한 항목 출력 및 제거
+        pair<int,int> cur = dq.front();
+        cout << cur.first << '\n';
+        
         dq.pop_front();
         
-        if(dq.empty())
-            break;
-        
-        if(cur > 0)
+        // #2. 양수 일 경우, 오른쪽 회전
+        if(cur.second > 0)
         {
-            cur--;
-            while(cur != 0)
+            cur.second--;
+            // cur.second = 양수 회전수, 즉 회전수가 1이 될 때까지 앞에 있는 항목을 덱의 마지막 위치로 재배치
+            while(cur.second > 0)
             {
                 dq.push_back(dq.front());
                 dq.pop_front();
-                cur--;
+                cur.second--;
             }
         }
+        // #3. 음수 일 경우, 왼쪽 회전
         else
         {
-            while(cur!=0)
+            // cur.second = 음수 회전수, 즉 회전수가 -1이 될 때까지 뒤에 있는 항목을 덱의 첫번째 위치로 재배치
+            while(cur.second < 0)
             {
                 dq.push_front(dq.back());
                 dq.pop_back();
-                cur++;
+                cur.second++;
             }
         }
     }
     
-    return 0;
+    
 }
