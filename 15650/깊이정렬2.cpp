050b91cc9ed395c7c3f920e1733054e088a6bd43
YongsulLee <sadghnmb04@gmail.com>
2023-11-27 21:11:01 +0900

    Feature : fix 15650, C++ 형식으로 수정 및 주석 추가

--- 15650/깊이정렬2.cpp
+++ 15650/깊이정렬2.cpp
@@ -1,31 +1,33 @@
 #include <iostream>
-#define MAX 9
+#include <vector>
 using namespace std;
 
-i nt n, m;
-int arr[MAX] = {
-    0,
-};
-bool visited[MAX] = {
-    0,
-};
+int N, M;
 
-void dfs(int num, int cnt)
+void DFS(int cnt, int cur, vector<int> &v, vector<bool> &visited)
 {
-    if (cnt == m)
+    // #1. M개의 수열이 완성되면, 정답 출력
+    if (cnt == M)
     {
-        for (int i = 0; i < m; i++)
-            cout << arr[i] << ' ';
+        for (int i = 0; i < M; i++)
+            cout << v[i] << ' ';
         cout << '\n';
         return;
     }
-    for (int i = num; i <= n; i++)
+
+    // #2. 'cur'을 통해 현재 레벨 부터 깊이 우선 탐색 수행
+    for (int i = cur; i <= N; i++)
     {
+        // #3. 방문 여부 체크
         if (!visited[i])
         {
+            // #4. 방문 여부 체크
             visited[i] = true;
-            arr[cnt] = i;
-            dfs(i + 1, cnt + 1);
+            // #5. 수열에 다음 방문 정점 추가
+            v[cnt] = i;
+            // #6. 수열의 다음 위치, 그리고 다음 레벨에 대한 깊이 우선 탐색 재귀 호출
+            DFS(cnt + 1, i + 1, v, visited);
+            // #7. 방문 여부 체크
             visited[i] = false;
         }
     }
@@ -33,6 +35,19 @@ void dfs(int num, int cnt)
 
 int main()
 {
-    cin >> n >> m;
-    dfs(1, 0);
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> N >> M;
+
+    //#1. 방문 여부
+    vector<bool> visited(N + 1, false);
+    //#2. 수열을 담을 vector 컨테이너
+    vector<int> v(M);
+
+    //#3. 깊이 우선 탐색 수행
+    DFS(0, 1, v, visited);
+
+    return 0;
 }
