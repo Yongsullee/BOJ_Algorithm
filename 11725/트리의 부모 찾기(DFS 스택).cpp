2f24ed9e6c8aac9900100b82759fa653d2855336
YongsulLee <sadghnmb04@gmail.com>
2023-11-27 22:14:45 +0900

    Feature : fix 11725, 트리를 2차원 벡터 대신 unordered_map으로 구현

--- 11725/트리의 부모 찾기(DFS 스택).cpp
+++ 11725/트리의 부모 찾기(DFS 스택).cpp
@@ -1,31 +1,43 @@
 #include <iostream>
-#include <stack>
+#include <unordered_map>
 #include <vector>
+#include <stack>
+#include <climits>
 using namespace std;
 
-vector<int> graph[100001];
-int ans[100001];
-bool visited[100001] = {0,};
+int N;
 
-void DFS(int start)
+void DFS(int cur, unordered_map<int, vector<int>> &tree, vector<int> &parent)
 {
+    // #1. stack 컨테이너 선언
     stack<int> s;
-    visited[100001] = {0,};
-    
-    s.push(start);
-    visited[start] = true;
-    
-    while(!s.empty())
+    //#2. 방문 여부
+    vector<bool> visited(tree.size() + 1, false);
+    // #3. 루트 노드를 stack에 삽입
+    s.push(cur);
+    // #4. 루트 노드의 방문여부 체크
+    visited[cur] = true;
+
+    while (!s.empty())
     {
+        // #1. stack에 가장 마지막으로 들어온 정점
         int cur = s.top();
+        // #2. stack에서 top 노드 제거
         s.pop();
-        for(const auto& next : graph[cur])
+        // #3. 현재 노드의 다음 레벨에 위치한 정점들 방문
+        if (tree.find(cur) != end(tree))
         {
-            if(!visited[next])
+            for (const auto &next : tree[cur])
             {
-                visited[next] = true;
-                ans[next] = cur;
-                s.push(next);
+                if (!visited[next])
+                {
+                    // #4. 방문 여부
+                    visited[next] = true;
+                    // #5. stack에 다음 레벨에 위치하며 현재 노드와 연결된 정점들 삽입
+                    s.push(next);
+                    // #6. 다음 레벨에 위치한 정점들의 부모 노드를 현재 노드로 업데이트
+                    parent[next] = cur;
+                }
             }
         }
     }
@@ -33,27 +45,31 @@ void DFS(int start)
 
 int main()
 {
-    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-    
-    int N;
-    
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
     cin >> N;
-    
-    for(int i=1; i<=N; i++)
+
+    // #1. unordered_map 형식으로 트리 선언
+    unordered_map<int, vector<int>> tree;
+    // #2. 부모 노드를 저장할 vector 컨테이너
+    vector<int> parent(N + 1, INT_MAX);
+
+    for (int i = 0; i < N - 1; i++)
     {
-        int x,y;
-        cin >> x;
-        cin >> y;
-        
-        graph[x].push_back(y);
-        graph[y].push_back(x);
+        int cur, next;
+        cin >> cur >> next;
+
+        // #2. 각 노드와 연결되어 있는 인접 정접들 추가
+        tree[cur].push_back(next);
+        tree[next].push_back(cur);
     }
-    
-    DFS(1);
-    
-    for(int i=2; i<=N; i++)
-    {
-        cout << ans[i] << '\n';
-    }
-    
+
+    DFS(1, tree, parent);
+
+    for (int i = 2; i <= N; i++)
+        cout << parent[i] << '\n';
+
+    return 0;
 }
