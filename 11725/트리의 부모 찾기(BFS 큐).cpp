d6b25979dbc0af0488d79d33c95b4d5a9e205ad7
Yongsullee <webdev_ys@kakao.com>
2023-11-29 23:55:50 +0900

    Feature : fix 11725_BFS, C++ 형식으로 재작성 및 주석 추가

--- 11725/트리의 부모 찾기(BFS 큐).cpp
+++ 11725/트리의 부모 찾기(BFS 큐).cpp
@@ -1,54 +1,71 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <unordered_map>
+#include <climits>
 #include <queue>
 using namespace std;
 
-vector<int> graph[100001];
-int ans[100001];
+int N;
 
-void BFS(int start)
+// 큐를 활용하는 너비 우선 탐색 정의
+void BFS(int start, unordered_map<int, vector<int>>& tree, vector<bool>& visited, vector<int>& parent)
 {
+    // 큐 선언
     queue<int> q;
-    bool visited[100001] = {0,};
-    
+    // 큐에 시작 노드 삽입
     q.push(start);
+    // 시작 노드 방문 여부 체크
     visited[start] = true;
     
     while(!q.empty())
     {
         int cur = q.front();
         q.pop();
-        for(const auto& neighbor : graph[cur])
+        
+        for(const auto& neighbor : tree[cur])
         {
             if(!visited[neighbor])
             {
                 visited[neighbor] = true;
-                ans[neighbor] = cur;
+                parent[neighbor] = cur;
                 q.push(neighbor);
             }
         }
     }
+    
 }
 
 int main()
 {
-    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
     
-    int N;
     cin >> N;
     
-    for(int i=1; i<=N; i++)
+    // unordered_map 형식의 트리 선언
+    unordered_map<int, vector<int>> tree;
+    // 방문 여부
+    vector<bool> visited(N+1, false);
+    // 부모 노드 목록
+    vector<int> parent(N+1, INT_MAX);
+    
+    for(int i=0; i<N-1; i++)
     {
-        int from, to;
-        cin >> from;
-        cin >> to;
+        int parent, child;
         
-        graph[to].push_back(from);
-        graph[from].push_back(to);
+        cin >> parent >> child;
+        
+        // 문제의 트리는 계층 구조가 분명하지 않아서, 무방향 간선으로 가정
+        tree[parent].push_back(child);
+        tree[child].push_back(parent);
     }
     
-    BFS(1);
+    BFS(1, tree, visited, parent);
     
-    for(int i=2; i<=N; i++)
-        cout << ans[i] << '\n';
+    for(int i=2; i<=N; ++i)
+        cout << parent[i] << '\n';
+    
+    return 0;
 }
