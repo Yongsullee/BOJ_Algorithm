039129ceb95b839d445dd5a29335df5bb721fe40
YongsulLee <sadghnmb04@gmail.com>
2024-01-21 20:40:03 +0900

    Fix : 11004번 문제 퀵 셀렉트 -> nth_element 함수로 변경

--- 11004/K번째 수(퀵 셀렉트).cpp
+++ 11004/K번째 수(퀵 셀렉트).cpp
@@ -1,47 +1,7 @@
-
-
 #include <iostream>
+#include <algorithm>
 #include <vector>
-#include <string>
 using namespace std;
-
-typedef long long ll;
-
-int N, K;
-
-int Partition(vector<ll> &arr, int l, int r)
-{
-    ll piv = arr[r];
-    int i = l - 1;
-
-    for (int j = l; j < r; ++j)
-    {
-        if (arr[j] <= piv)
-        {
-            ++i;
-            swap(arr[j], arr[i]);
-        }
-    }
-
-    swap(arr[i + 1], arr[r]);
-
-    return i + 1;
-}
-
-int QuickSelect(vector<ll> &arr, int l, int r, int k)
-{
-    if (l == r)
-        return arr[l];
-
-    int pivotIndex = Partition(arr, l, r);
-
-    if (k == pivotIndex)
-        return arr[k];
-    else if (k < pivotIndex)
-        return QuickSelect(arr, l, pivotIndex - 1, k);
-    else
-        return QuickSelect(arr, pivotIndex + 1, r, k);
-}
 
 int main()
 {
@@ -49,15 +9,17 @@ int main()
     cin.tie(0);
     cout.tie(0);
 
+    int N, K;
     cin >> N >> K;
 
-    vector<ll> v(N);
+    vector<int> v(N);
 
     for (int i = 0; i < N; ++i)
         cin >> v[i];
 
-    // k번째 수 출력
-    cout << QuickSelect(v, 0, N - 1, K - 1);
+    nth_element(v.begin(), v.begin() + K - 1, v.end());
+
+    cout << v[K - 1];
 
     return 0;
 }
